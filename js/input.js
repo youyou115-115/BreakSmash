5809bced724c2958ec2b56d083cc082be6543576
youyou115-115 <gimu.gimu.x@gmail.com>
2026-08-03 16:53:56 +0900

    Update input.js

--- js/input.js
+++ js/input.js
@@ -1,41 +1,92 @@
-window.addEventListener(
-"click",
-()=>{
+const Input = {
 
+
+init(canvas){
+
+
+    canvas.addEventListener(
+        "pointerdown",
+        (e)=>{
+
+
+            const rect = canvas.getBoundingClientRect();
+
+
+            // 実際の画面座標
+
+            const x =
+            e.clientX - rect.left;
+
+
+            const y =
+            e.clientY - rect.top;
+
+
+
+            const scale = Math.min(
+                canvas.width / 800,
+                canvas.height / 700
+            );
+
+
+
+            const offsetX =
+            (canvas.width - 800 * scale) / 2;
+
+
+            const offsetY =
+            (canvas.height - 700 * scale) / 2;
+
+
+
+            // 仮想800×700へ変換
+
+            const gameX =
+            (x - offsetX) / scale;
+
+
+            const gameY =
+            (y - offsetY) / scale;
+
+            // タイトル画面
 
 if(Game.screen==="title"){
 
-
     Game.start();
 
+    return;
 
 }
-else if(Game.screen==="play"){
 
 
-    if(ObjectManager.objects.length>0){
+
+            // クリック判定
+
+            for(let obj of ObjectManager.objects){
 
 
-        const obj =
-        ObjectManager.objects[0];
+                if(
+                    gameX > obj.x &&
+                    gameX < obj.x + obj.width &&
+                    gameY > obj.y &&
+                    gameY < obj.y + obj.height
+                ){
+
+                    obj.damage();
+
+                    break;
+
+                }
 
 
-        obj.damage();
-
-
-        if(obj.isBroken()){
-
-
-            Game.score += obj.score;
+            }
 
 
         }
-
-
-    }
+    );
 
 
 }
 
 
-});
+};
